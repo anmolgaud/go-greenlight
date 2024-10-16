b5be92f072d1b04170a7fe022f636f9d56d1ecbb
anmolgaud <anmol.gaud@convegenius.ai>
2024-10-16 15:27:19 +0530

    half/chapter 3.5

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"greenlight.anmol.gaud/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -16,5 +19,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID: id,
+		CreatedAt: time.Now(),
+		Title: "Batman",
+		Runtime: 102,
+		Genre: []string{"drama", "action", "thriller"},
+		Version: 1,
+	}
+
+	err = app.writeJson(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
 }
---
+++ internal/data/movies.go
@@ -0,0 +1,15 @@
+package data 
+
+import (
+	"time"
+)
+
+type Movie struct {
+	ID int64 `json:"id"`
+	Title string `json:"title"`
+	Year int32 `json:"year,omitempty"`
+	Runtime int32 `json:"runtime,omitempty"`
+	Genre []string `json:"genre,omitempty"`
+	Version int32 `json:"version"`
+	CreatedAt time.Time `json:"-"`
+}
