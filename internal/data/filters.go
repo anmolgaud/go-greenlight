14438c175fcb0811b34fe533718709ba948c3be1
anmolgaud <anmol.gaud@convegenius.ai>
2024-10-24 23:58:32 +0530

    chapter 9

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,25 @@
 package data
 
-import "greenlight.anmol.gaud/internal/validator"
+import (
+	"math"
+	"strings"
+
+	"greenlight.anmol.gaud/internal/validator"
+)
 
 type Filters struct {
 	Page     int
 	PageSize int
 	Sort     string
 	SortSafelist []string
+}
+
+type Metadata struct {
+	CurrentPage int `json:"currentPage,omitempty"`
+	PageSize int `json:"pageSize,omitempty"`
+	FirstPage int `json:"firstPage,omitempty"`
+	LastPage int `json:"lastPage,omitempty"`
+	TotalRecords int `json:"totalRecords,omitempty"`
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
@@ -16,4 +29,41 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+};
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+	return Metadata{
+		CurrentPage: page,
+		PageSize: pageSize,
+		FirstPage: 1,
+		LastPage: int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
 }
