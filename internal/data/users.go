cfff03b07524d5bf110e9a5ce2c202687f584cff
anmolgaud <anmol.gaud@convegenius.ai>
2024-11-03 01:36:22 +0530

    chapter 15

--- internal/data/users.go
+++ internal/data/users.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"time"
+	"crypto/sha256"
 
 	"golang.org/x/crypto/bcrypt"
 	"greenlight.anmol.gaud/internal/validator"
@@ -169,4 +170,44 @@ func (m UserModel) Update(user *User) error {
 		}
 	}
 	return nil
+}
+
+func (m UserModel) GetForToken(tokenScope, tokenPlaintext string) (*User, error) {
+	tokenHash := sha256.Sum256([]byte(tokenPlaintext))
+	query := `
+	SELECT
+		users.id,
+		users.name,
+		users.email,
+		users.password_hash,
+		users.activated,
+		users.created_at,
+		users.version
+	FROM users
+	INNER JOIN tokens
+	ON users.id = tokens.user_id
+	WHERE tokens.hash = $1 AND tokens.scope = $2 AND tokens.expiry > $3;`
+	args := []any{tokenHash[:], tokenScope, time.Now()}
+
+	var user User
+	ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
+	defer cancel()
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.CreatedAt,
+		&user.Version,
+	)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+	return &user, nil
 }
