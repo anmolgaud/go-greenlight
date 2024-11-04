fd1b2135de14628216ce0aa149097329298dae7d
anmolgaud <anmol.gaud@convegenius.ai>
2024-11-04 16:27:26 +0530

    chapter 17

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -48,10 +48,15 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
-
+	err = app.models.Permissions.AddForUser(user.ID, "movies:read")
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 	token, err := app.models.Tokens.New(user.ID, 3 * time.Hour, data.ScopeActivation)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
+		return
 	}
 	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user, "token": token.Plaintext}, nil)
 	if err != nil {
